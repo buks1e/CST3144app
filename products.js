558527524ab2ec83bcebf98024802c77f7b9daa6
Ibukun <ibukundurodola@gmail.com>
2024-11-26 15:05:52 +0400

    Added Petdepot front end

---
+++ products.js
@@ -0,0 +1,49 @@
+let products = [
+    {
+        id:1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresitable</em>,"+" organic goodness for your cat",
+        price: 2000,
+        image:"images/product-fullsize.png",
+        availableInv: 10,
+        rating: 0
+    },
+    {
+        id:1002,
+        title: "Litter Box",
+        description: "A Neat litter box for your cat",
+        price: 3000,
+        image:"images/product-fullsize.png",
+        availableInv: 10,
+        rating: 0
+    },
+    {
+        id:1003,
+        title: "Yarn",
+        description: "A nice ball of yarn that will keep your cat entertained for hours",
+        price: 500,
+        image:"images/product-fullsize.png",
+        availableInv: 10,
+        rating: 0
+    },
+    {
+        id:1004,
+        title: "Cat Collar",
+        description: "A cute little cat collar to identify and decorate your cat with",
+        price:2000,
+        image:"images/product-fullsize.png",
+        availableInv: 10,
+        rating: 0
+    },
+    {
+        id:1005,
+        title: "Canned Food",
+        description: "Can of delicious canned cat food packed with nutrients to keep your cat healthy",
+        price: 250,
+        image:"images/product-fullsize.png",
+        availableInv: 25,
+        rating: 0
+    },
+
+
+]
